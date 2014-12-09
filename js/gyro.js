bf0050b0821710b6301d27aa39234ad346af9aea
Anders Stalheim Øfsdahl <anders@apt.no>
2014-12-09 17:00:25 +0100

    Use requestAnimationFrame instead of setInterval
    Added polyfill for older browsers

--- js/gyro.js
+++ js/gyro.js
@@ -7,7 +7,41 @@
  * @license MIT License
  * @options frequency, callback
  */
-(function (root, factory) {
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ 
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+ 
+// MIT license
+ 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
+ (function (root, factory) {
 		if (typeof define === 'function' && define.amd) {
 				// AMD. Register as an anonymous module.
 				define(factory);
@@ -44,6 +78,9 @@
 			features = [];
 
 	var gyro = {};
+	var animation_id = false;
+	var animation_start = false;
+	var callback = false;
 
 	/**
 	 * @public
@@ -59,15 +96,24 @@
 	gyro.getOrientation = function() {
 		return measurements;
 	};
-
-	gyro.startTracking = function(callback) {
-		interval = setInterval(function() {
+	
+	gyro.step = function(timestamp) {
+		if (!animation_start) animation_start = timestamp;
+		if (timestamp - animation_start >= gyro.frequency) {
+			animation_start = timestamp;
 			callback(measurements);
-		}, gyro.frequency);
+		}
+		animation_id = window.requestAnimationFrame(gyro.step);
+	};
+	
+	gyro.startTracking = function(cbk) {
+		callback = cbk;
+		animation_id = window.requestAnimationFrame(gyro.step);
 	};
 
 	gyro.stopTracking = function() {
-		clearInterval(interval);
+		window.cancelAnimationFrame(animation_id);
+		animation_start = false;
 	};
 
 	/**
